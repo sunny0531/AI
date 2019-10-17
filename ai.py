bed39269628552b176e4c78bb77a18095c17d6a7
sunny0531 <37855944+sunny0531@users.noreply.github.com>
2019-10-17 20:41:45 +0800

    Update ai.py

--- ai.py
+++ ai.py
@@ -5,12 +5,28 @@ from tkinter.filedialog import askopenfilename
 from google.cloud import automl_v1beta1
 from google.cloud.automl_v1beta1.proto import service_pb2
 from google.cloud.automl_v1beta1 import PredictionServiceClient
+from PIL import Image, ImageTk
 def chose_file():
-    result_l["text"]=""
+    forget_l()
+    result_l.pack()
+    result_l["text"]="loading image"
     file_path = askopenfilename()
+    print(file_p)
     with open(file_path, 'rb') as ff:
         content = ff.read()
+        img_r=Image.open(file_path)
+        img=img_r.load()
+        img_tk=ImageTk.PhotoImage(img_r)
+        #w,h=img.getpixel(("x","y"))
+    label = tk.Label(window, image=img_tk)
+    label.image=img_tk
+    label.pack()
+    with open(file_p, 'rb') as ff:
+        content = ff.read()
+        img_r=Image.open(file_path)
+        img=img_r.load()
     response=get_prediction(content, project_id,  model_id)
+    result_l["text"]="no result"
     for result in response.payload:
         result_l["text"]=result.display_name
         break
@@ -27,6 +43,4 @@ result_l=tk.Label(window)
 summit_b=tk.Button(window,text="choose and summit",command=chose_file).pack()
 project_id = "aivic-253700"
 model_id = "ICN1725724822968560826"
-#file_path = "td.jpg"
-result_l.pack()
 window.mainloop()
